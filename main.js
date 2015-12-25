59230577e166e1a4f92e1ac31286bc4804fa276f
klipspringr <mari.tir3sias@gmail.com>
2015-12-25 01:26:48 -0800

    Add titles

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 var picList = null;
-
 //
 //		GALLERY VIEW
 //
 
-function getData(query) {
+function getData() {
 	var request = new XMLHttpRequest();
 	var url = "https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=f0acf8413c19cafc668726c43a93c6cf&format=json&safe_search=1&content_type=1&sort=relevance&extras=url_t,url_z&page=1&per_page=25&text=kittens&nojsoncallback=1";
 
@@ -36,15 +35,16 @@ function displayData(response) {
 		image.setAttribute("src", picList[i].url_t);
 		image.setAttribute("id", i);
 		image.setAttribute("class", "thumbnail")
-		image.setAttribute("onclick", "imageClick('" + picList[i].url_z + "'," + i + ")");
+		image.setAttribute("onclick", "imageClick('" + picList[i].url_z + "'," + i + ",'" + picList[i].title + "')");
 		container.appendChild(image);
 	}
 }
 
-function imageClick(url, id) {
+function imageClick(url, id, title) {
 	document.getElementById("container-popup").style.display = "block";
 	document.getElementById("lightboxImage").setAttribute("src", url);
 	document.getElementById("lightboxImage").setAttribute("alt", id);
+	document.getElementById("image-title").textContent =  title;
 }
 
 //
@@ -55,6 +55,10 @@ function getLargeImageUrl(id) {
 	return picList[id].url_z;
 }
 
+function getImageTitle(id) {
+	return picList[id].title;
+}
+
 function exitLightbox() {
 	document.getElementById("container-popup").style.display = "none";
 }
@@ -63,18 +67,23 @@ function rightArrow() {
 	var lightbox = document.getElementById("lightboxImage");
 	var newID = parseInt(lightbox.alt) + 1;
 	var nextImageUrl = getLargeImageUrl(newID);
+	var title = document.getElementById("image-title");
 	// TO-DO: Add check to see if it's at the end of the list
 
 	lightbox.setAttribute("src", nextImageUrl);
 	lightbox.setAttribute("alt", newID);
+	title.textContent = getImageTitle(newID);
 }
 
 function leftArrow() {
 	var lightbox = document.getElementById("lightboxImage");
 	var newID = parseInt(lightbox.alt) - 1;
 	var nextImageUrl = getLargeImageUrl(newID);
+	var title = document.getElementById("image-title");
 	// TO-DO: Add check to see if it's at the end of the list
 
 	lightbox.setAttribute("src", nextImageUrl);
 	lightbox.setAttribute("alt", newID);
+	title.textContent = getImageTitle(newID);
+
 }
