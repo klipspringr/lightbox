970fe67f61e63912af7594c895ad84eb0a58f7ab
klipspringr <mari.tir3sias@gmail.com>
2015-12-26 00:14:51 -0800

    Fix photo bug, add search bar

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var picList = null;
 var lightboxDiv = document.getElementById("container-lightbox");
 var lightboxImage = document.getElementById("lightbox-image");
 var imageTitle = document.getElementById("image-title");
+var loadingText = document.getElementById("loading-text");
 
 //
 //		GALLERY VIEW
@@ -9,30 +10,40 @@ var imageTitle = document.getElementById("image-title");
 
 function retrieveData() {
     var request = new XMLHttpRequest();
-    var url = "https://www.flickr.com/services/rest/?method=flickr.photos.search" + 
-    		  "&api_key=f0acf8413c19cafc668726c43a93c6cf&format=json&safe_search=1&" +
-    		  "content_type=1&sort=relevance&extras=url_q,url_z&page=1&per_page=28&text=kittens" +
-    		  "&nojsoncallback=1";
+    var query = document.getElementById("query").value.trim();
+    var thumbnailContainer = document.getElementById("container-thumbnails");
+    if (query) {
+	    var url = "https://www.flickr.com/services/rest/?method=flickr.photos.search" + 
+	    		  "&api_key=f0acf8413c19cafc668726c43a93c6cf&format=json&safe_search=1&" +
+	    		  "content_type=1&sort=relevance&extras=url_q&page=1&per_page=28&nojsoncallback=1" +
+	    		  "&text=" + query;
 
-    request.open('GET', url, true);
+	    loadingText.style.display = "block";
+	    
+	    while (thumbnailContainer.firstChild) {
+	    	thumbnailContainer.removeChild(thumbnailContainer.firstChild);
+	    }
 
-    request.onload = function() {
-        if (request.status >= 200 && request.status < 400) {
-            // Data has been successfully retrieved.
-            var response = JSON.parse(request.responseText);
-            displayThumbnails(response);
-        } else {
-            // The server has returned an error.
-            console.log("The server has returned an error.");
-        }
-    };
+	    request.open('GET', url, true);
 
-    request.onerror = function() {
-        // The connection has returned an error.
-        console.log("The connection has returned an error.");
-    };
+	    request.onload = function() {
+	        if (request.status >= 200 && request.status < 400) {
+	            // Data has been successfully retrieved.
+	            var response = JSON.parse(request.responseText);
+	            displayThumbnails(response);
+	        } else {
+	            // The server has returned an error.
+	            console.log("The server has returned an error.");
+	        }
+	    };
 
-    request.send();
+	    request.onerror = function() {
+	        // The connection has returned an error.
+	        console.log("The connection has returned an error.");
+	    };
+
+	    request.send();    	
+    }
 }
 
 function displayThumbnails(response) {
@@ -44,18 +55,19 @@ function displayThumbnails(response) {
         thumbnail.setAttribute("src", picList[i].url_q);
         thumbnail.setAttribute("id", i);
         thumbnail.setAttribute("class", "thumbnail");
-        thumbnail.setAttribute("onclick", "openLightbox('" + picList[i].url_z + "'," 
-        	+ i + ',"' + picList[i].title + '")');
+        thumbnail.setAttribute("onclick", "openLightbox('" + picList[i].url_q + "'," 
+        	+ i + ",'" + picList[i].title.replace(/'/g, "\\'") + "')");
         thumbnailContainer.appendChild(thumbnail);
     }
-    document.getElementById("loading-text").style.display = "none";
+    loadingText.style.display = "none";
 }
 
 function openLightbox(url, id, title) {
     lightboxDiv.style.display = "block";
-    lightboxImage.setAttribute("src", url);
+    lightboxImage.setAttribute("src", url.replace("_q.jpg", "_z.jpg"));
     lightboxImage.setAttribute("alt", id);
 
+    checkID(lightboxImage.alt);
     imageTitle.textContent = title;
 }
 
@@ -64,17 +76,33 @@ function openLightbox(url, id, title) {
 //
 
 function getImageUrl(id) {
-    return picList[id].url_z;
+    return picList[id].url_q.replace("_q.jpg", "_z.jpg");
 }
 
 function getImageTitle(id) {
     return picList[id].title;
 }
 
+function checkID(id) {
+	var rightButton = document.getElementById("right-button");
+	var leftButton = document.getElementById("left-button");
+
+	if (id == 0){
+		rightButton.style.display = "block";
+		leftButton.style.display = "none";
+	} else if (id == 27){
+		rightButton.style.display = "none";
+		leftButton.style.display = "block";
+	} else {
+		rightButton.style.display = "block";
+		leftButton.style.display = "block";
+	}
+}
+
 function exitLightbox() {
     lightboxDiv.style.display = "none";
-        lightboxImage.setAttribute("src", null);
-        lightboxImage.setAttribute("alt", null);
+    lightboxImage.setAttribute("src", "");
+    lightboxImage.setAttribute("alt", "");
 }
 
 function nextImage() {
@@ -84,7 +112,9 @@ function nextImage() {
         lightboxImage.setAttribute("src", getImageUrl(newID));
         lightboxImage.setAttribute("alt", newID);
         imageTitle.textContent = getImageTitle(newID);
-    }
+    } 
+
+    checkID(lightboxImage.alt);
 }
 
 function previousImage() {
@@ -95,4 +125,6 @@ function previousImage() {
         lightboxImage.setAttribute("alt", newID);
         imageTitle.textContent = getImageTitle(newID);
     }
+
+    checkID(lightboxImage.alt);
 }
