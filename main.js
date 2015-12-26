90ba7c0d428381579f84dd036d48139a29e56495
klipspringr <mari.tir3sias@gmail.com>
2015-12-25 19:39:01 -0800

    Clean code, change thumbnail style

--- main.js
+++ main.js
@@ -1,89 +1,98 @@
 var picList = null;
+var lightboxDiv = document.getElementById("container-lightbox");
+var lightboxImage = document.getElementById("lightbox-image");
+var imageTitle = document.getElementById("image-title");
+
 //
 //		GALLERY VIEW
 //
 
-function getData() {
-	var request = new XMLHttpRequest();
-	var url = "https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=f0acf8413c19cafc668726c43a93c6cf&format=json&safe_search=1&content_type=1&sort=relevance&extras=url_t,url_z&page=1&per_page=25&text=kittens&nojsoncallback=1";
+function retrieveData() {
+    var request = new XMLHttpRequest();
+    var url = "https://www.flickr.com/services/rest/?method=flickr.photos.search" + 
+    		  "&api_key=f0acf8413c19cafc668726c43a93c6cf&format=json&safe_search=1&" +
+    		  "content_type=1&sort=relevance&extras=url_q,url_z&page=1&per_page=28&text=kittens" +
+    		  "&nojsoncallback=1";
 
-	request.open('GET', url, true);
+    request.open('GET', url, true);
 
-	request.onload = function() {
-	  if (request.status >= 200 && request.status < 400) {
-	    // Success!
-	    var resp = JSON.parse(request.responseText);
-	    displayData(resp);
-	  } else {
-	    // We reached our target server, but it returned an error
-	  }
-	};
+    request.onload = function() {
+        if (request.status >= 200 && request.status < 400) {
+            // Data has been successfully retrieved.
+            var response = JSON.parse(request.responseText);
+            displayThumbnails(response);
+        } else {
+            // The server has returned an error.
+            console.log("The server has returned an error.");
+        }
+    };
 
-	request.onerror = function() {
-	  // There was a connection error of some sort
-	};
-	request.send();
+    request.onerror = function() {
+        // The connection has returned an error.
+        console.log("The connection has returned an error.");
+    };
 
+    request.send();
 }
 
-function displayData(response) {
-	picList = response.photos.photo;
-	var container = document.getElementById("container-thumbnails");
+function displayThumbnails(response) {
+    picList = response.photos.photo;
+    var thumbnailContainer = document.getElementById("container-thumbnails");
 
-	for (var i in picList){
-		var image = document.createElement("img");
-		image.setAttribute("src", picList[i].url_t);
-		image.setAttribute("id", i);
-		image.setAttribute("class", "thumbnail")
-		image.setAttribute("onclick", "imageClick('" + picList[i].url_z + "'," + i + ",'" + picList[i].title + "')");
-		container.appendChild(image);
-	}
+    for (var i in picList) {
+        var thumbnail = document.createElement("img");
+        thumbnail.setAttribute("src", picList[i].url_q);
+        thumbnail.setAttribute("id", i);
+        thumbnail.setAttribute("class", "thumbnail");
+        thumbnail.setAttribute("onclick", "openLightbox('" + picList[i].url_z + "'," 
+        	+ i + ',"' + picList[i].title + '")');
+        thumbnailContainer.appendChild(thumbnail);
+    }
+    document.getElementById("loading-text").style.display = "none";
 }
 
-function imageClick(url, id, title) {
-	document.getElementById("container-popup").style.display = "block";
-	document.getElementById("lightboxImage").setAttribute("src", url);
-	document.getElementById("lightboxImage").setAttribute("alt", id);
-	document.getElementById("image-title").textContent =  title;
+function openLightbox(url, id, title) {
+    lightboxDiv.style.display = "block";
+    lightboxImage.setAttribute("src", url);
+    lightboxImage.setAttribute("alt", id);
+
+    imageTitle.textContent = title;
 }
 
 //
 //		LIGHTBOX VIEW
 //
 
-function getLargeImageUrl(id) {
-	return picList[id].url_z;
+function getImageUrl(id) {
+    return picList[id].url_z;
 }
 
 function getImageTitle(id) {
-	return picList[id].title;
+    return picList[id].title;
 }
 
 function exitLightbox() {
-	document.getElementById("container-popup").style.display = "none";
+    lightboxDiv.style.display = "none";
+        lightboxImage.setAttribute("src", null);
+        lightboxImage.setAttribute("alt", null);
 }
 
-function rightArrow() {
-	var lightbox = document.getElementById("lightboxImage");
-	var newID = parseInt(lightbox.alt) + 1;
-	var nextImageUrl = getLargeImageUrl(newID);
-	var title = document.getElementById("image-title");
-	// TO-DO: Add check to see if it's at the end of the list
+function nextImage() {
+    var newID = parseInt(lightboxImage.alt) + 1;
 
-	lightbox.setAttribute("src", nextImageUrl);
-	lightbox.setAttribute("alt", newID);
-	title.textContent = getImageTitle(newID);
+    if (newID < 28) {
+        lightboxImage.setAttribute("src", getImageUrl(newID));
+        lightboxImage.setAttribute("alt", newID);
+        imageTitle.textContent = getImageTitle(newID);
+    }
 }
 
-function leftArrow() {
-	var lightbox = document.getElementById("lightboxImage");
-	var newID = parseInt(lightbox.alt) - 1;
-	var nextImageUrl = getLargeImageUrl(newID);
-	var title = document.getElementById("image-title");
-	// TO-DO: Add check to see if it's at the end of the list
+function previousImage() {
+    var newID = parseInt(lightboxImage.alt) - 1;
 
-	lightbox.setAttribute("src", nextImageUrl);
-	lightbox.setAttribute("alt", newID);
-	title.textContent = getImageTitle(newID);
-
+    if (newID > -1) {
+        lightboxImage.setAttribute("src", getImageUrl(newID));
+        lightboxImage.setAttribute("alt", newID);
+        imageTitle.textContent = getImageTitle(newID);
+    }
 }
